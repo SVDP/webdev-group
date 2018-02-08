31c58f7dbc850d6b86915bd4e70a6e03e61109fb
Samuel <SVDP@github.com>
2018-02-08 15:02:22 +0100

    HTML 1

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,5 +1,355 @@
 @component('layouts.app')
-    <header class="flex items-center justify-center min-h-screen">
-        <span class="text-4xl">🥄</span>
-    </header>
+    <section>
+        <div class="">
+            <!-- header start -->
+            <header>
+                <div class="">
+                    <img src="" alt="" />
+                </div>
+                <div class="">
+                    <div class="">
+                        <div class="">
+                            <h1>PHP Antwerp</h1>
+                            <span>All in the same boat since 2015</span>
+                        </div>
+                        <div class="">
+                            <nav>
+                                <ul>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                    <div class="">
+                        <div class="">
+                            <h2>About PHP Antwerp</h2>
+                        </div>
+                        <div class="">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </p>
+                        </div>
+                        <div class="">
+                            <h2>Get in touch</h2>
+                        </div>
+                        <div class="">
+                            <div class="">
+                                <p>
+                                    Info@phpantwerp.be
+                                    <br />
+                                    @phpantwerp
+                                </p>
+                            </div>
+                            <div class="">
+                                <p>
+                                    github.com/phpantwerp
+                                    <br />
+                                    meetup.com/phpantwerp
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <!-- header end -->
+        </div>
+    </section>
+    <section>
+        <main>
+            <!-- upcoming meetups start -->
+            <div class="">
+                <section>
+                    <div class="">
+                        <h2>Upcoming events</h2>
+                    </div>
+                    <div class="">
+                        <div class="">
+                            <div class="">
+                                <span>#14</span>
+                                <span>
+                                    Monday 13 februari 2017
+                                    <br />
+                                    19h00
+                                </span>
+                            </div>
+                            <div class="">
+                                <span>Alfred E. Neumann, Vince Geraldi</span>
+                            </div>
+                            <!--<button type="button" name="talk">I want to give a talk</button>-->
+                            <div class="">
+                                <span>
+                                    Spotcrowd
+                                    <br />
+                                    Melkmarkt 3, Antwerp
+                                </span>
+                            </div>
+                            <!--<button type="button" name="help">I can help with a venue</button>-->
+                            <button type="button" name="join">Join us</button>
+                        </div>
+                        <div class="">
+                            <div class="">
+                                <span>#14</span>
+                                <span>
+                                    Monday 13 februari 2017
+                                    <br />
+                                    19h00
+                                </span>
+                            </div>
+                            <div class="">
+                                <span>Alfred E. Neumann, Vince Geraldi</span>
+                            </div>
+                            <!--<button type="button" name="talk">I want to give a talk</button>-->
+                            <div class="">
+                                <span>
+                                    Spotcrowd
+                                    <br />
+                                    Melkmarkt 3, Antwerp
+                                </span>
+                            </div>
+                            <!--<button type="button" name="help">I can help with a venue</button>-->
+                            <button type="button" name="join">Join us</button>
+                        </div>
+                        <div class="">
+                            <div class="">
+                                <span>#14</span>
+                                <span>
+                                    Monday 13 februari 2017
+                                    <br />
+                                    19h00
+                                </span>
+                            </div>
+                            <div class="">
+                                <span>Alfred E. Neumann, Vince Geraldi</span>
+                            </div>
+                            <!--<button type="button" name="talk">I want to give a talk</button>-->
+                            <div class="">
+                                <span>
+                                    Spotcrowd
+                                    <br />
+                                    Melkmarkt 3, Antwerp
+                                </span>
+                            </div>
+                            <!--<button type="button" name="help">I can help with a venue</button>-->
+                            <button type="button" name="join">Join us</button>
+                        </div>
+                        <div class="">
+                            <div class="">
+                                <span>#14</span>
+                                <span>
+                                    Monday 13 februari 2017
+                                    <br />
+                                    19h00
+                                </span>
+                            </div>
+                            <div class="">
+                                <span>Alfred E. Neumann, Vince Geraldi</span>
+                            </div>
+                            <!--<button type="button" name="talk">I want to give a talk</button>-->
+                            <div class="">
+                                <span>
+                                    Spotcrowd
+                                    <br />
+                                    Melkmarkt 3, Antwerp
+                                </span>
+                            </div>
+                            <!--<button type="button" name="help">I can help with a venue</button>-->
+                            <button type="button" name="join">Join us</button>
+                        </div>
+                    </div>
+                </section>
+            </div>
+            <!-- upcoming meetups end -->
+            <!-- past meetups start -->
+            <section>
+                <div class="">
+                    <div class="">
+                        <img src="" alt="" />
+                    </div>
+                    <div class="">
+                        <img src="" alt="" />
+                    </div>
+                </div>
+                <div class="">
+                    <div class="">
+                        <div class="">
+                            <h4>
+                                <span>#13 — 21 december 2016</span>
+                                <br />
+                                hosted by MCS
+                            </h4>
+                        </div>
+                        <div class="">
+                            <div class="">
+                                <h5>Speakers</h5>
+                                <p>
+                                    Alfred E. Neumann
+                                    <br />
+                                    Vince Geraldi
+                                </p>
+                            </div>
+                            <div class="">
+                                <h5>Feedback</h5>
+                                <p>
+                                    Feedback on Meetup
+                                    <br />
+                                    Joined.in
+                                    <br />
+                                    GitHub
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="">
+                        <div class="">
+                            <h4>
+                                <span>#13 — 21 december 2016</span>
+                                <br />
+                                hosted by MCS
+                            </h4>
+                        </div>
+                        <div class="">
+                            <div class="">
+                                <h5>Speakers</h5>
+                                <p>
+                                    Alfred E. Neumann
+                                    <br />
+                                    Vince Geraldi
+                                </p>
+                            </div>
+                            <div class="">
+                                <h5>Feedback</h5>
+                                <p>
+                                    Feedback on Meetup
+                                    <br />
+                                    Joined.in
+                                    <br />
+                                    GitHub
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- past meetups end -->
+            <!-- participate start -->
+            <section>
+                <div class="">
+                    <div class="">
+                        <h2>Participate</h2>
+                    </div>
+                    <div class="">
+                        <div class="">
+                            <h3>Attend a meetup</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            </p>
+                        </div>
+                        <div class="">
+                            <h3>Host a meetup</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            </p>
+                        </div>
+                        <div class="">
+                            <h3>Give a talk</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            </p>
+                        </div>
+                        <div class="">
+                            <h3>Sponsor us</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            </p>
+                        </div>
+                        <div class="">
+                            <img src="" alt="" />
+                        </div>
+                        <div class="">
+                            <img src="" alt="" />
+                        </div>
+                        <div class="">
+                            <img src="" alt="" />
+                        </div>
+                        <div class="">
+                            <img src="" alt="" />
+                        </div>
+                        <div class="">
+                            <img src="" alt="" />
+                        </div>
+                        <div class="">
+                            <img src="" alt="" />
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- participate end -->
+        </main>
+    </section>
+    <section>
+        <div class="">
+            <!-- footer start -->
+            <footer>
+                <div class="">
+                    <div class="">
+                        <h2>Organized by</h2>
+                    </div>
+                    <div class="">
+                        <div class="">
+                            <div class="">
+                                <img src="" alt="" />
+                            </div>
+                            <span>
+                                Alfred E. Neumann
+                                <br />
+                                @alfredeneumann
+                            </span>
+                        </div>
+                        <div class="">
+                            <div class="">
+                                <img src="" alt="" />
+                            </div>
+                            <span>
+                                Alfred E. Neumann
+                                <br />
+                                @alfredeneumann
+                            </span>
+                        </div>
+                        <div class="">
+                            <div class="">
+                                <img src="" alt="" />
+                            </div>
+                            <span>
+                                Alfred E. Neumann
+                                <br />
+                                @alfredeneumann
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="">
+                    <div class="">
+                        <h2>Proudly sponsored by</h2>
+                    </div>
+                    <div class="">
+                        <img src="" alt="" />
+                        <img src="" alt="" />
+                        <img src="" alt="" />
+                        <img src="" alt="" />
+                        <img src="" alt="" />
+                        <img src="" alt="" />
+                        <img src="" alt="" />
+                        <img src="" alt="" />
+                        <img src="" alt="" />
+                    </div>
+                </div>
+                <div class="">
+                    <small>copyright</small>
+                </div>
+            </footer>
+            <!-- footer end -->
+        </div>
+    </section>
 @endcomponent
